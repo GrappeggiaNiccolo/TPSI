d9e156456a5d3f42395ff1aa3045aa97234de808
Grappeggia Niccolò <niccolo.grappeggia@iisviolamarchesini.edu.it>
2023-02-08 10:23:35 +0100

    Modifica esercizi

--- vari/stringa.c
+++ vari/stringa.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 void main()
 {
-    char stringa[30];
+    char str[30];
+    int i = 0;
+    printf("Inserisci una stringa: ");
+    scanf("%s", str); // non si usa & perchè è un array
+    printf("\nHai inserito la segurnte stringa %s", str);
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    printf("\nLa stringa è lunga %d caratteri", i);
 }
