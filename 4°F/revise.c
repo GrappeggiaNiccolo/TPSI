cd7b8b27d6aadafb912f48f5c0db30a7ed46b8d2
Grappeggia Niccolò <niccolo.grappeggia@iisviolamarchesini.edu.it>
2023-09-14 09:00:27 +0200

    ripasso

---
+++ 4°F/revise.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{   // input 2 valori limite if(compresi fra 30 e 50) --> output valori; input 5 int in "n",  print somma if (n > limite1 && n < limite 2)
+    int limite1, limite2, n;
+
+
+    return 0;
+}
