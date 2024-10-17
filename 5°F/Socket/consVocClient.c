27fa193a8ee7d3a04a788f63d06bde7980433ce5
Niccolò Grappeggia <niccolo.grappeggia@iisviolamarchesini.edu.it>
2024-10-17 23:55:34 +0200

    Conta vocali e consonanti con socket

--- 5°F/Socket/consVocClient.c
+++ 5°F/Socket/consVocClient.c
@@ -12,8 +12,43 @@
 #include <unistd.h>
 
 #define DIM 256
-#define SERVERPORT 15021
+#define SERVERPORT 1025
 
-int main() {
-    
+void main()
+{
+    // variabili
+    struct sockaddr_in server;
+    int socketfd; // identificatore socket
+
+    char str1[DIM];
+    int numVoc, numCons;
+    // inizializzazione socket
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(SERVERPORT);
+
+    // creazione socket
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    // connessione al server
+    connect(socketfd, (struct sockaddr *)&server, sizeof(server));
+    printf("\nInserisci una stringa: ");
+    scanf("%s", str1);
+
+    // invio stringa al server
+    write(socketfd, str1, sizeof(str1));
+
+    // lettura dal server
+    read(socketfd, &numVoc, sizeof(numVoc));
+    printf("\nnumero vocali: %d\n", numVoc);
+    read(socketfd, &numCons, sizeof(numCons));
+    printf("\nnumero consonanti: %d\n", numCons);
+
+    // chiusura socket
+    close(socketfd);
 }
--- 5°F/Socket/consVocServer.c
+++ 5°F/Socket/consVocServer.c
@@ -12,9 +12,70 @@
 #include <unistd.h>
 
 #define DIM 256
-#define SERVERPORT 15021
+#define SERVERPORT 1025
 
-void main() {
+void ContaConsVoc(char str[], int *numVoc, int *numCons)
+{
+    char vocali[] = "aeiouAEIOU";
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (strchr(vocali, str[i]))
+        {
+            (*numVoc)++;
+        }
+        else if (isalpha(str[i]))
+        {
+            (*numCons)++;
+        }
+    }
+}
+
+void main()
+{
     // Variabili
     struct sockaddr_in server, client;
+    int socketfd, connessione, length = sizeof(server);
+
+    char str[DIM];
+    int numVoc = 0, numCons = 0;
+    // inizializzazione socket
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(SERVERPORT);
+
+    // creazione socket
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    // bind
+    bind(socketfd, (struct sockaddr *)&server, sizeof(server));
+
+    // listen
+    listen(socketfd, 1);
+
+    while (1)
+    {
+        printf("\nServer in attesa\n");
+
+        // pulisco il canale
+        fflush(stdout);
+
+        // accept
+        connessione = accept(socketfd, (struct sockaddr *)&client, &length);
+
+        // lettura
+        read(connessione, str, sizeof(str));
+        printf("\nStringa ricevuta: %s\n", str);
+
+        ContaConsVoc(str, &numVoc, &numCons);
+
+        write(connessione, &numVoc, sizeof(numVoc));
+        write(connessione, &numCons, sizeof(numCons));
+
+        close(connessione);
+    }
 }
